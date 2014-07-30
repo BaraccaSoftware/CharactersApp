ed3a15169fbebab5a8c87dbba166d57539f2b39b
Giovanni Lorenzo Napoleoni <giovannilorenzo.napoleoni@gmail.com>
2014-07-30 21:30:08 +0200

    First commit

---
+++ src/com/pigmal/android/ex/twitter4j/Const.java
@@ -0,0 +1,16 @@
+package com.pigmal.android.ex.twitter4j;
+
+public class Const {
+	static String CONSUMER_KEY = "EyKRJoSIlhFNIL2Qn9bxi0NX0";
+	static String CONSUMER_SECRET = "C0Lw1q05WbXylbuuNWNyJ5R5OSfQSB5rIkFnUhvjAHK281Hzcd";
+
+	static String PREFERENCE_NAME = "twitter_oauth";
+	static final String PREF_KEY_SECRET = "oauth_token_secret";
+	static final String PREF_KEY_TOKEN = "oauth_token";
+	
+	static final String CALLBACK_URL = "oauth://t4jsample";
+	
+	static final String IEXTRA_AUTH_URL = "auth_url";
+	static final String IEXTRA_OAUTH_VERIFIER = "oauth_verifier";
+	static final String IEXTRA_OAUTH_TOKEN = "oauth_token";
+}
